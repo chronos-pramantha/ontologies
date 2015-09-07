4237e5a1f165320fe9eff14e2ea6aa1ed4c8b705
Lorenzo <tunedconsulting@gmail.com>
2015-09-07 16:59:57 +0200

    change in vocabulary

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='ChronosLOD',
-      version='0.2',
-      description='Chronos Ontologies Handler',
+      version='0.3',
+      description='Chronos RDF vocabularies Handler',
       author='lorenzo',
       author_email='lorenzo@projectchronos.org',
       url='http://www.python.org/sigs/distutils-sig/',
